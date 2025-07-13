b600e2a429b07a80a5f1e001257f9e253f34dfce
Bethlehem-Tesfaye <bethlehem.tesfaye15@gmail.com>
2025-07-13 13:23:09 +0400

    change to connectionString

--- server/db/db.js
+++ server/db/db.js
@@ -5,11 +5,7 @@ import pg from 'pg'
 const {Pool} = pg
 
 const conn = new Pool({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    port:process.env.DB_PORT,
-    password:process.env.DB_PASS,
-    database:process.env.DB_NAME,
+    connectionString: process.env.DATABASE_URL,
 })
 
 export default conn
