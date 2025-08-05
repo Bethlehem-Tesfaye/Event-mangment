e93c78cca832fe02271eb6a7d7715c2bfe22b268
Bethlehem-Tesfaye <bethlehem.tesfaye15@gmail.com>
2025-08-05 13:20:06 +0400

    refactor(api): remove success field from API responses

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -2,7 +2,6 @@
 const errorMiddleware = async (err, req, res, _next) => {
   const statusCode = err.statusCode || 500;
   res.status(statusCode).json({
-    success: false,
     message: err.message || "Server Error"
   });
 };
