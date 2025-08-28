6bcaf194bfc60c51e6f2250ce162f541c61a8d63
Bethlehem-Tesfaye <bethlehem.tesfaye15@gmail.com>
2025-08-28 15:39:19 +0400

    fix: test config

--- server/jest.config.mjs
+++ server/jest.config.mjs
@@ -1,28 +1,13 @@
 export default {
   testEnvironment: "node",
-  transform: {},
-  clearMocks: true,
-  collectCoverage: true,
+  transform: {}, 
+  clearMocks: true, 
   coverageDirectory: "coverage",
-  extensionsToTreatAsEsm: [".js"],
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1"
   },
-  testEnvironment: "node",
-  moduleFileExtensions: [
-    "js",
-    "mjs",
-    "cjs",
-    "jsx",
-    "ts",
-    "mts",
-    "cts",
-    "tsx",
-    "json",
-    "node"
-  ],
   testMatch: [
     "**/__tests__/**/*.?([mc])[jt]s?(x)",
     "**/?(*.)+(spec|test).?([mc])[jt]s?(x)"
   ]
-}; 
+};
