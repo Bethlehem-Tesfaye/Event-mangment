8f6c6150b8f6efcec4c3f1cf8379dc0eca67ada8
Bethlehem-Tesfaye <bethlehem.tesfaye15@gmail.com>
2025-09-10 11:03:03 +0400

    fix: nameing adn theme fix

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,9 +1,5 @@
-import lightning from "./lightning.png";
-import calander from "./calendar.png";
-import ticket from "./ticket.png";
-import presentation from "./presentation.png";
-import street from "./street-map.png";
-import banner from "./placard.png";
-import confetti from "./confetti.png";
+import lightning from './lightning.png'
 
-export { lightning, calander, presentation, ticket, street, confetti, banner };
+export {
+    lightning
+}
