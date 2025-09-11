219e6a8d1f0ef95ee9d344c37f7c88b9390df1c7
Bethlehem-Tesfaye <bethlehem.tesfaye15@gmail.com>
2025-09-11 20:18:11 +0400

    fix: change the pulseloader

--- frontend/src/components/custom/PulseLoader.tsx
+++ frontend/src/components/custom/PulseLoader.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import lightning from "@/assets/lightning.png"; // or your Logo component
 
 interface PulseLoaderProps {
   show: boolean;
@@ -8,19 +9,12 @@ const PulseLoader: React.FC<PulseLoaderProps> = ({ show }) => {
   if (!show) return null;
 
   return (
-    <div className="fixed inset-0 bg-black/30 backdrop-blur-[0px] flex items-center justify-center z-50">
-      <svg
-        className="animate-pulse"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 256 512"
-        width="200"
-        height="200"
-      >
-        <path
-          d="M216 160h-92l36-128-120 192h92l-36 160 120-224h-92z"
-          fill="black"
-        />
-      </svg>
+    <div className="fixed inset-0 bg-black/30 backdrop-blur-[0px]  flex items-center justify-center z-50">
+      <img
+        src={lightning}
+        alt="logo"
+        className="w-20 h-20 animate-pulse brightness-125 drop-shadow-lg"
+      />
     </div>
   );
 };
