766238e7fc6babc35e6a7a5b7c9fd19a03d54827
Bethlehem-Tesfaye <bethlehem.tesfaye15@gmail.com>
2025-09-12 23:06:59 +0400

    feat: ui structire for t he event browse and preview page

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import { LoginPage } from "@/features/auth/pages/LoginPage";
 import { RegisterPage } from "@/features/auth/pages/RegisterPage";
 import Dashboard from "@/features/dashboard/Dashboard";
+import SplashScreen from "../components/SplashScreen";
+import Events from "@/features/event/pages/Events";
+import { EventPreview } from "@/features/event/pages/EventPreview";
 
 export const router = createBrowserRouter([
   {
@@ -19,5 +22,13 @@ export const router = createBrowserRouter([
   {
     path: "/dashboard",
     element: <Dashboard />,
+  },
+  {
+    path: "/browse-event",
+    element: <Events/>,
+  },
+  {
+    path: "/events/:id",
+    element: <EventPreview/>,
   },
 ]);
