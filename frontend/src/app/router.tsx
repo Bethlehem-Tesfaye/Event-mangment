445f5c1c6a05c414f4b209e9fd42acb02bfd541a
Bethlehem-Tesfaye <bethlehem.tesfaye15@gmail.com>
2025-09-12 23:06:59 +0400

    feat: ui structire for t he event browse and preview page

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -50,4 +50,12 @@ export const router = createBrowserRouter([
     path: "/events/:id",
     element: <EventPreview/>,
   },
+  {
+    path: "/browse-event",
+    element: <Events/>,
+  },
+  {
+    path: "/events/:id",
+    element: <EventPreview/>,
+  },
 ]);
