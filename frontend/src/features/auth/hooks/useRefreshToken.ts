f4bf8d589923723e0f77af3a0625843aec14a415
Bethlehem-Tesfaye <bethlehem.tesfaye15@gmail.com>
2025-09-14 03:13:48 +0400

    fix: remane to useRefreshAuthToken

--- frontend/src/features/auth/hooks/useRefreshToken.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { refreshTokens, type RefreshResponse } from "../api/refreshToken";
-
-export const useRefresh = () =>
-  useQuery<RefreshResponse, Error>({
-    queryKey: ["auth", "refresh"],
-    queryFn: refreshTokens,
-    retry: false,
-  });
