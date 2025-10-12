4b2952f3e57691e2bc03550104736b6df5365f1d
Adonias Mulugeta <mulugeta.adonias@gmail.com>
2025-10-12 11:36:49 +0300

    Fix rebase issues

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import { LoginPage } from "@/features/auth/pages/LoginPage";
 import { RegisterPage } from "@/features/auth/pages/RegisterPage";
-import SplashScreen from "../components/SplashScreen";
 import Events from "@/features/event/pages/Events";
 import { EventPreview } from "@/features/event/pages/EventPreview";
 
